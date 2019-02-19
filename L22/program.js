aa080f413f6ba898296159a490cd24f279342ee7
kyz-aiaidawa <aiaidawa@aim.com>
2019-02-19 16:24:36 +0900

    第５章終わりです。 	modified:   L22/index.html 	modified:   L22/program.js 	new file:   L25/index.html 	new file:   L25/program.js 	modified:   five.md 	new file:   six.md

--- L22/program.js
+++ L22/program.js
@@ -5,3 +5,17 @@ function kurikaeshi(){
     }
     alert(kekka);
 }
+function keisan(){
+    kekka = 0;
+    text01 = document.getElementById('text01');
+    x = parseInt(text01.value);
+
+    ketasuu = 0;
+    do {
+        x = x / 10;
+        x = Math.floor(x);
+        ketasuu = ketasuu + 1;
+    } while (x != 0);
+    alert(ketasuu);
+    
+}
